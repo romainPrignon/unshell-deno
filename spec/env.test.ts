b0d809d5ff161d857fcacacb98963547a23ec2a3
romainprignon <pro.rprignon@gmail.com>
2022-02-23 20:09:28 +0100

    add env test

--- spec/env.test.ts
+++ spec/env.test.ts
@@ -1,3 +1,21 @@
+import { assertEquals } from "../deps.ts"
+
+import unshell, { exec } from '../src/mod.ts'
 
 // Deno.env.set("QUX", "qux")
 // await echo(Deno.env.get("QUX"))()
+
+Deno.test(
+  `should be able to print env var througth js`,
+  async () => {
+    // Given
+    Deno.env.set("QUX", "qux")
+    const { echo } = unshell()
+
+    // When
+    const res = await exec(echo(Deno.env.get("QUX")))
+
+    // Then
+    assertEquals(res, "qux\n") // TODO
+  }
+)
