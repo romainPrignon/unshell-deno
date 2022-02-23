52dfe07c2a173fb4e4305ec09e802c9135728054
romainprignon <pro.rprignon@gmail.com>
2022-02-23 07:51:00 +0100

    add exec spec

--- spec/exec.test.ts
+++ spec/exec.test.ts
@@ -1,2 +1,38 @@
 // test restore
 
+import { assert, assertEquals } from "../deps.ts"
+
+import unshell, { exec } from '../src/mod.ts'
+
+Deno.test(
+  `given echo bin, if we call it twice, then there should be no overlap between command`,
+  async () => {
+    // Given
+    const { echo } = unshell()
+
+    // When
+    const foo = await exec(echo('foo'))
+    const bar = await exec(echo('bar'))
+
+    // Then
+    assertEquals(foo, 'foo\n')
+    assertEquals(bar, 'bar\n')
+  }
+)
+
+Deno.test(
+  `given echo bin, if we mix the calls, then there should be no overlap between command`,
+  async () => {
+    // Given
+    const { echo } = unshell()
+
+    // When
+    const echoFoo = echo('foo')
+    const bar = await exec(echo('bar'))
+    const foo = await exec(echoFoo)
+
+    // Then
+    assertEquals(foo, 'foo\n')
+    assertEquals(bar, 'bar\n')
+  }
+)
