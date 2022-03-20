3b85ab1185695cb6c70accdfb73217e3d9dd1e09
romainprignon <pro.rprignon@gmail.com>
2022-03-20 21:41:48 +0100

    handle error using cmd status code

--- src/resolve.ts
+++ src/resolve.ts
@@ -2,13 +2,13 @@
 import type { Process } from "../type/index.d.ts"
 
 export const resolve = async (process: Process) => {
-  // const status = await process.status()
+  const { success } = await process.status()
   const stdout = new TextDecoder("utf-8").decode(await process.output())
   const stderr = new TextDecoder("utf-8").decode(await process.stderrOutput())
 
   process.close()
 
-  if (stderr) throw new Error(stderr.trim()) // todo: better error shape
+  if (!success) throw new Error(stderr.trim())
   // return process?.stdout && readLines(process?.stdout) // TODO
 
   return stdout.trim()
